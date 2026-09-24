1a2d0e186b5157ba4bcd45f46f3a87c4e3ae764f
NickJuneau <nickjuneau63@gmail.com>
2026-09-24 16:55:16 -0400

    added dev limit for rows printed; added try/except;

--- services/replay/main.py
+++ services/replay/main.py
@@ -1,18 +1,33 @@
 from services.replay.parser import normalizeRow
 import csv, json, time, random
 
+CSV_PATH = 'data/online-retail-dataset.csv'
+MAX_EVENTS = 20 # Set to None for full run
+
 def main():
-    with open(file='data/online-retail-dataset.csv', mode='r', encoding='utf-8-sig') as file:
+    count = 0
+
+    with open(file=CSV_PATH, mode='r', encoding='utf-8-sig') as file:
         reader = csv.DictReader(file)
 
         for row in reader:
-            rowDict = normalizeRow(row)
-            jsonStr = json.dumps(rowDict)
+            try:
+                rowDict = normalizeRow(row)
+                jsonStr = json.dumps(rowDict)
 
-            print(jsonStr)
+                print(jsonStr)
 
-            # Will stream a new transaction between every x and y seconds
-            time.sleep(random.uniform(.1, 1))
+                count+=1
+                # If MAX_EVENTS is None it will skip the if statement (Short Cicruit)
+                if MAX_EVENTS and count >= MAX_EVENTS:
+                    print(f"\nReached dev limit of {MAX_EVENTS} transactions. Exiting...")
+                    break
+
+                # Will stream a new transaction between every x and y seconds
+                time.sleep(random.uniform(.1, 1))
+            except Exception as e:
+                print(f"Error processing row: {e}")
+                continue
 
 if __name__ == "__main__":
     main()
